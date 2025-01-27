2a5cdedf76d7f6b97a524c378a03d69d99d3860c
Srajan Bansal <srajanbansal999@gmail.com>
2025-01-27 17:38:35 +0530

    Added Problems type

--- packages/common/zod/src/types.ts
+++ packages/common/zod/src/types.ts
@@ -9,3 +9,29 @@ export const UserLoginSchema = z.object({
 	email: z.string().email(),
 	password: z.string().min(5).max(20),
 });
+
+export const SubmissionInputSchema = z.object({
+	code: z.string(),
+	languageId: z.number(),
+	problemId: z.number(),
+});
+
+const DifficultySchema = z.enum(['easy', 'medium', 'hard']);
+
+const ProblemTypeSchema = z.enum([
+	'Array',
+	'String',
+	'HashTable',
+	'LinkedList',
+]);
+
+export const ProblemSchema = z.object({
+	id: z.string(),
+	title: z.string(),
+	slug: z.string(),
+	statement: z.string(),
+	difficulty: DifficultySchema,
+	type: ProblemTypeSchema,
+});
+
+export type ProblemType = z.infer<typeof ProblemSchema>;
